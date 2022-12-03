492ce6eea96391dcf5ea2d587bfff8bf1895a673
jenish Dhanani <202112106@daiict.ac.in>
2022-12-03 12:58:56 +0530

    new deploy changes

--- index.js
+++ index.js
@@ -511,14 +511,21 @@ app.get('/api/dashboard',async(req,res)=>{
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"))
     const path = require("path");
+    app.use(express.static(path.join(__dirname,'./client/build')))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.join(__dirname,'./client/build/index.html'))
     })
 }
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("client/build"))
+//     const path = require("path");
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
 
 app.listen(PORT,()=>{
-    console.log(`server run sucessfully ${process.env.PORT || 5000}👍`);
+    console.log(`server run sucessfully ${process.env.PORT}👍`);
 })
 
